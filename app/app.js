e9517b38dce1319d666075a887768a0054f7812a
unknown <Andreas@andreas09.west.surikat.net>
2015-03-09 19:29:09 +0100

    Fixed vertical / horizontal split boxes, started looking at drag and drop

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@ var angular = require('angular');
 
 var boxModule = angular.module('ui.box', []);
 
-boxModule.directive('hbox', function() {
+var _makeDirective = function(direction) {
+	var _modeHorizontal = direction==='horizontal';
 	return {
 		restrict: 'E',
 		replace: true,
@@ -14,78 +15,150 @@ boxModule.directive('hbox', function() {
 		template: '<div class="splitbox"><div class="splitter ng-resize"><div> </div></div><div ng-transclude-replace/></div>',
 		controller: function($scope) {
 		},
-		link: function(scope, element, attr) {
-			
-			var _isDragging = false;
-			var _modeHorizontal = element.prop('tagName') === 'HBOX';
-			console.log(element.prop('tagName'));
+		link:  function(scope, element, attr) {
 
-			var splitter = element.children().eq(0);
-			var one = element.children().eq(1);
-			var two = element.children().eq(2);
-			element.addClass(_modeHorizontal ? 'horizontal' : 'vertical');
-			one.addClass('splitboxbox');
-			two.addClass('splitboxbox');
-			element.bind('mousemove', function(event) {
-				if(!_isDragging) {
-					return;
-				} else {
-					var bounds = element[0].getBoundingClientRect();
-            var height = bounds.bottom - bounds.top;
-            pos = event.clientY - bounds.top;
+	var _isDragging = false;
+
+	var splitter = element.children().eq(0);
+	var one = element.children().eq(1);
+	var two = element.children().eq(2);
+	element.addClass(_modeHorizontal ? 'horizontal' : 'vertical');
+	splitter.addClass(_modeHorizontal ? 'horizontal' : 'vertical');
+	one.addClass('splitboxbox');
+	two.addClass('splitboxbox');
+	
+	element.bind('mousemove', function(event) {
+		if(!_isDragging) {
+			return;
+		} else {
+			var bounds = element[0].getBoundingClientRect();
 
 /*            if (pos < pane1Min) return;
-            if (height - pos < pane2Min) return;
-*/
-            splitter.css('top', pos + 'px');
-            one.css('height', pos + 'px');
-            two.css('top', pos + 'px');			
-				}
+    if (height - pos < pane2Min) return;
+*/	
+			console.log(_modeHorizontal);
+			var pct = 0;
+			if(_modeHorizontal){
+			    var height = bounds.bottom - bounds.top;
+			    var pos = event.clientY - bounds.top;
+				pct = pos/height*100;
+		    	splitter.css('top',pct + '%');
+		    	one.css('height', pct + '%');
+			    two.css('top', pct + '%');			
+			} else {
+			    var width = bounds.right - bounds.left;
+			    var pos = event.clientX - bounds.left;
+				pct = pos/width*100;
+				   splitter.css('left',pct + '%');
+			    one.css('width', pct + '%');
+		    	two.css('left', pct + '%');			
 
-			});
-			splitter.bind('mousedown', function(event) {
-				event.preventDefault();
-				_isDragging = true;
-			});
-			angular.element(document).bind('mouseup', function(event) {
-				_isDragging = false;
-			});
+			}
+			console.log(pct + "%");
+		}
+	});
+	splitter.bind('mousedown', function(event) {
+		event.preventDefault();
+		_isDragging = true;
+	});
+	angular.element(document).bind('mouseup', function(event) {
+		_isDragging = false;
+	});
 
 //			var firstBounds = one.getBoundingClientRect();
 //			console.log(one);
-			var _init = function() {
-				splitter.css('top', 50 + '%');
-				two.css('top', 50 + '%');
-			}
-			_init();
-
+	var _init = function() {
+		if(_modeHorizontal) {
+			one.css('height', '50%')
+			splitter.css('top', 50 + '%');
+			two.css('top', 50 + '%');
+		} else{
+			splitter.css('left', 50 + '%');
+			two.css('left', 50 + '%');
+			one.css('width', '50%')
 		}
+	}
+	_init();
+}
 
 	}
+}
+
+
+boxModule.directive('hbox', function() {
+	return _makeDirective('horizontal');
 });
 
+boxModule.directive('vbox', function() {
+	return _makeDirective('vertical');
+});
+
+
 boxModule.directive('ngTranscludeReplace', ['$log', function ($log) {
-              return {
-                  terminal: true,
-                  restrict: 'EA',
+      return {
+          terminal: true,
+          restrict: 'EA',
 
-                  link: function ($scope, $element, $attr, ctrl, transclude) {
-                      if (!transclude) {
-                          $log.error('orphan',
-                                     'Illegal use of ngTranscludeReplace directive in the template! ' +
-                                     'No parent directive that requires a transclusion found. ');
-                          return;
-                      }
-                      transclude(function (clone) {
-                          if (clone.length) {
-                              $element.replaceWith(clone);
-                          }
-                          else {
-                              $element.remove();
-                          }
-                      });
+          link: function ($scope, $element, $attr, ctrl, transclude) {
+              if (!transclude) {
+                  $log.error('orphan',
+                             'Illegal use of ngTranscludeReplace directive in the template! ' +
+                             'No parent directive that requires a transclusion found. ');
+                  return;
+              }
+              transclude(function (clone) {
+                  if (clone.length) {
+                      $element.replaceWith(clone);
                   }
-              };
-          }]);
+                  else {
+                      $element.remove();
+                  }
+              });
+          }
+      };
+  }]);
 
+boxModule.directive('window', function () {
+      return {
+		restrict: 'E',
+		replace: true,
+		transclude: true,
+		scope: {
+			orientation: '@'
+		},
+		template: '<div class="window"><div ng-transclude></div></div>',
+		controller: function($scope) {
+		},
+		link:  function(scope, element, attr) {
+			element.bind('drop', function(event) {
+				console.log(event);
+				console.log('ondragend');
+			});
+			element.bind('dragover', function(event) {
+				event.preventDefault();
+				//console.log('dragging');
+			});
+		}
+      };
+  })	;
+
+boxModule.directive('pane', function () {
+      return {
+		restrict: 'E',
+		replace: true,
+		transclude: true,
+		scope: {
+			orientation: '@'
+		},
+		template: '<div class="window" draggable="true"><div ng-transclude></div></div>',
+		controller: function($scope) {
+		},
+		link:  function(scope, element, attr) {
+			element.bind('dragstrat', function() {
+				console.log('dragstart');
+				event.dataTransfer.setData('text', 'Hello World!');
+			});
+		}
+      };
+  })	;
 
