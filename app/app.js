892945aeb86b801d31eff567b252907bef80412d
Andreas Karlsson <sprawl@onlajn.nu>
2015-03-13 22:00:46 +0100

    started to look at drag / drop events

--- app/app.js
+++ app/app.js
@@ -36,7 +36,6 @@ var _makeDirective = function(direction) {
 /*            if (pos < pane1Min) return;
     if (height - pos < pane2Min) return;
 */	
-			console.log(_modeHorizontal);
 			var pct = 0;
 			if(_modeHorizontal){
 			    var height = bounds.bottom - bounds.top;
@@ -54,7 +53,6 @@ var _makeDirective = function(direction) {
 		    	two.css('left', pct + '%');			
 
 			}
-			console.log(pct + "%");
 		}
 	});
 	splitter.bind('mousedown', function(event) {
@@ -118,18 +116,52 @@ boxModule.directive('ngTranscludeReplace', ['$log', function ($log) {
       };
   }]);
 
+windowController.$inject = ['$scope'];
+
+function windowController($scope) {
+	var ctrl = this;
+	var panes = ctrl.panes = $scope.panes = [];
+	ctrl.select = function(selectedPane) {
+		angular.forEach(panes, function(pane) {
+			if(pane.active && pane !== selectedPane) {
+				pane.active = false;
+				pane.onDeselect();
+			}
+		});
+		selectedPane.active = true;
+		selectedPane.onSelect();
+	}
+
+	ctrl.addPane = function(pane) {
+		console.log('add pane ');
+		console.log(pane);
+		panes.push(pane);
+		ctrl.select(pane);
+	}
+	ctrl.removePane = function(pane) {
+		var index = panes.indexOf(pane);
+		if(pane.active && panes.length > 1) {
+			 var newActiveIndex = index == panes.length - 1 ? index - 1 : index + 1;
+			ctrl.select(panes[newActiveIndex]);
+		}
+		panes.splice(index, 1);
+	}
+}
+
+boxModule.controller('WindowController', windowController);
+
 boxModule.directive('window', function () {
       return {
 		restrict: 'E',
 		replace: true,
 		transclude: true,
+		require:'window',
 		scope: {
 			orientation: '@'
 		},
 		template: '<div class="window"><div ng-transclude></div></div>',
-		controller: function($scope) {
-		},
-		link:  function(scope, element, attr) {
+		controller: 'WindowController',
+		link:  function(scope, element, attr, windowController) {
 			var splitArea = $('body').children('.splitdroparea');
 			if(!splitArea.length) {
 				splitArea = $('<div class="splitdroparea" style="display:none;"></div>');
@@ -139,15 +171,27 @@ boxModule.directive('window', function () {
 
 
 			element.bind('drop', function(event) {
-				console.log(event);
-				console.log(event.dataTransfer);
-				console.log(event.originalEvent.dataTransfer.types);
 				console.log('drop');
+				console.log(event);
+				console.log(event.originalEvent.dataTransfer.types);
+				var area = getAreaFromEvent(event);
+				console.log(event.target);
+				console.log(element);
+				console.log(area);
+				var scp = angular.element(event.target).scope();
+				console.log(scp);
+				windowController.addPane(scp);
+				console.log(windowController);
+				console.log('/drop');
 			});
 			element.bind('dragend', function(event) {
-				console.log(event);
 				console.log('dragend');
+				console.log(event);
 				splitArea.hide();
+				//successfully dropped?
+				if(event.originalEvent.dataTransfer.dropEffect !== 'none') {
+					console.log('dropped');
+				}
 			});
 			element.bind('dragenter', function(event) {
 				event.stopPropagation();
@@ -166,57 +210,114 @@ boxModule.directive('window', function () {
 //				console.log(event.clientY);
 				var height = bounds.bottom - bounds.top;
 				var width = bounds.right - bounds.left;
-			    var posY = event.originalEvent.clientY - bounds.top;
-			    var posX = event.originalEvent.clientX - bounds.left;
-				var pctY = posY/height;
-				var pctX = posX/width;
-//				console.log(pctX/pctY);
-				//calculate slopes
-				var oAD = pctX/pctY>1;
-				var oCB = pctX/(1-pctY)<=1;
 
 				var offset = element.offset();
-				if(oAD) {
-					if(oCB) {
-							splitArea.offset({top:offset.top, left:offset.left});
-							splitArea.width(width);
-							splitArea.height(height/2);
-					} else {
-							splitArea.offset({top:offset.top, left:offset.left+width/2});
-							splitArea.width(width/2);
-							splitArea.height(height);
-					}
-				} else {
-					if(oCB) {
-							splitArea.offset({top:offset.top, left:offset.left});
-							splitArea.width(width/2);
-							splitArea.height(height);
-					} else {
-							splitArea.offset({top:offset.top+height/2, left:offset.left});
-							splitArea.width(width);
-							splitArea.height(height/2);
-					}
+				var area = getAreaFromEvent(event);
+				switch(area) {
+					case AREAS.TOP:
+						splitArea.offset({top:offset.top, left:offset.left});
+						splitArea.width(width);
+						splitArea.height(height/2);
+						break;	
+					case AREAS.RIGHT:
+						splitArea.offset({top:offset.top, left:offset.left+width/2});
+						splitArea.width(width/2);
+						splitArea.height(height);
+						break;
+					case AREAS.LEFT:
+						splitArea.offset({top:offset.top, left:offset.left});
+						splitArea.width(width/2);
+						splitArea.height(height);
+						break;
+					case AREAS.BOTTOM:
+						splitArea.offset({top:offset.top+height/2, left:offset.left});
+						splitArea.width(width);
+						splitArea.height(height/2);
+						break;
 				}
 			});
 		}
       };
-  })	;
+  });
+
+var AREAS = {
+	TOP:'top', 
+	LEFT:'left', 
+	RIGHT:'right', 
+	BOTTOM:'bottom'
+}
+
+function getAreaFromEvent(event) {
+	var target = event.currentTarget;
+	var bounds = target.getBoundingClientRect();				
+
+    var posY = event.originalEvent.clientY - bounds.top;
+    var posX = event.originalEvent.clientX - bounds.left;
+    return getArea(bounds, posX, posY);
+}
+
+
+function getArea(bounds, posX, posY) {
+	var height = bounds.bottom - bounds.top;
+	var width = bounds.right - bounds.left;
+	var pctY = posY/height;
+	var pctX = posX/width;
+	//calculate slopes
+	var oAD = pctX/pctY>1;
+	var oCB = pctX/(1-pctY)<=1;
+	if(oAD) {
+		if(oCB) {
+			return AREAS.TOP;
+		} else {
+			return AREAS.RIGHT;
+		}
+	} else {
+		if(oCB) {
+			return AREAS.LEFT;
+		} else {
+			return AREAS.BOTTOM;
+		}
+	}
+}
 
 boxModule.directive('pane', function () {
       return {
 		restrict: 'E',
+		require:'^window',
 		replace: true,
 		transclude: true,
 		scope: {
-			orientation: '@'
+			caption: '@',
+			active: '=?',
+			onSelect:'&select',
+			onDeselect: '&deselect'
 		},
-		template: '<div class="window" draggable="true"><div ng-transclude></div></div>',
+		controller: function() {},
+		template: '<div class="pane" draggable="true"><div ng-transclude></div></div>',
 		controller: function($scope) {
 		},
-		link:  function(scope, element, attr) {
+/*		compile: function(element, attr, transclusion) {
+			return 
+		},*/
+		link:  function(scope, element, attr, windowCtrl) {
+
+			windowCtrl.addPane(scope);
+			scope.$watch('active', function(active) {
+          		if (active) {
+		            windowCtrl.select(scope);
+    	    	}
+       		});
+       		scope.select = function() {
+       			scope.active = true;
+       		}
+			scope.$on('$destroy', function() {
+				windowCtrl.removePane(scope);
+			});
+			console.log("CONTROL:");
+			console.log(windowCtrl);
 			element.bind('dragstart', function() {
 				console.log('dragstart');
-				event.dataTransfer.setData('text/plain', 'Hello World!');
+				event.dataTransfer.setData('application/x-lx-pane', 'Hello World!');
 			});
 		}
       };
